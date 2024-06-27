eb4860d0b6b5f61e336c04abe8a0d1181961307a
samonvercel <samonvercel7777@gmail.com>
2024-06-27 12:51:38 +0700

    update swiper

--- script.js
+++ script.js
@@ -89,11 +89,15 @@ dropBtns.forEach((btn, index) => {
 
 //Slide
 var swiper = new Swiper(".cubeSwiper2", {
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 10,
   freeMode: true,
   navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
   breakpoints: {
+    432: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
     768: {
       slidesPerView: 3,
       spaceBetween: 20,
